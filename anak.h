0a295aa35e2b021b9c1fcc0efe9c58037f50e61a
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-11 10:07:20 +0700

    membuat variabel, beserta constructor dan destructor

--- anak.h
+++ anak.h
@@ -2,6 +2,13 @@
 #define ANAK_H
 
 class anak {
-
+public:
+    string nama;
+    anak(string pNama) :nama(pnama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+     ~anak(){
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+     }
 };
 #endif
